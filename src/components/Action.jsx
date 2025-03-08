dc49815c12cf6063e47d80cb67ebb4d014c2e78e
iam-zeenat-khan <92390852+iam-zeenat-khan@users.noreply.github.com>
2025-03-08 15:44:00 +0530

    updated

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,6 +1,8 @@
-import { FaRegStar } from "react-icons/fa";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaRegStar, FaRegBell } from "react-icons/fa";
 import { IoMdAdd } from "react-icons/io";
-import { FaRegBell } from "react-icons/fa6";
 import { CiCalendar } from "react-icons/ci";
 import { BsRepeat } from "react-icons/bs";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
@@ -8,49 +10,84 @@ import { PiTrashFill } from "react-icons/pi";
 import { ImCross } from "react-icons/im";
 import { useSelector } from "react-redux";
 
-
-
 const Action = () => {
-    const isProfileVisible = useSelector(state => state.responsive.isProfileVisible)
-    const isActionVisible = useSelector(state => state.responsive.isActionVisible)
-    return (
-        <div className={`${!isProfileVisible && isActionVisible ? "ml-[23vw] col-span-2 bg-[#EEF6EF] pt-[10vh]  dark:bg-black dark:text-white " : "bg-[#EEF6EF] pt-[10vh]  dark:bg-black dark:text-white dark:border-solid-white"}`}>
-            <div className=" dark:bg-black dark:text-white ">
-                <ul className="pl-[3vh] pt-[3vh] space-y-8">
-                    <li className="border-t-2  flex  justify-between pt-[3vh] ">
+  const isProfileVisible = useSelector(state => state.responsive.isProfileVisible);
+  const isActionVisible = useSelector(state => state.responsive.isActionVisible);
+  
+  const [dueDate, setDueDate] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
-                        <div className="flex space-x-8 "> <h1 className="text-2xl">
-                            <MdOutlineCheckBoxOutlineBlank />
-                        </h1>
-
-                            <h4 className="">Buy groceries</h4></div>
-
-                        <h1 className="text-2xl pr-[4vw]"><FaRegStar /></h1>
-                    </li>
-                    <li className="border-t-2 flex  space-x-8 p"><h1 className="text-2xl"> <IoMdAdd /> </h1>
-                        <h4>Add Step</h4></li>
-                    <li className="border-t-2 flex space-x-8 "> <h1 className="text-2xl"> <FaRegBell /></h1>
-                        <h4>Set Reminder</h4></li>
-                    <li className="border-t-2 flex space-x-8 "> <h1 className="text-2xl"><CiCalendar /></h1><h4>Add Due Date</h4></li>
-                    <li className="border-t-2 flex space-x-8 "> <h1 className="text-2xl"><BsRepeat /></h1><h4>Repeat</h4></li>
-
-                    <li className="border-t-2 flex space-x-16 pb-[35vh] "><input className="bg-transparent pl-[2vw] pt-[2vh]  outline-none" placeholder="Add Notes" /></li>
-                    <li className="border-t-2 flex space-x-8 "> <h1 className="text-2xl"><BsRepeat /></h1></li>
-                     
-                </ul>
-
+  return (
+    <div className={`${!isProfileVisible && isActionVisible ? "ml-[23vw] col-span-2" : ""} bg-[#EEF6EF] pt-[10vh] min-h-screen dark:bg-black dark:text-white`}>
+      <div className="px-6">
+        <ul className="space-y-6">
+          {/* Task Title */}
+          <li className="border-t flex justify-between items-center pt-6">
+            <div className="flex space-x-4 items-center">
+              <MdOutlineCheckBoxOutlineBlank className="text-2xl" />
+              <h4 className="text-lg font-medium">Buy groceries</h4>
             </div>
-            <div className=" border-t-2 flex justify-between p-[1vh]">
-                <h1><ImCross />
-                </h1>
-                <h4> Created Today</h4>
-                <h1 className="text-2xl"><PiTrashFill />
-                </h1>
+            <FaRegStar className="text-2xl text-gray-500 hover:text-yellow-500 cursor-pointer" />
+          </li>
 
+          {/* Add Step */}
+          <li className="border-t flex items-center space-x-4 pt-4">
+            <IoMdAdd className="text-2xl" />
+            <h4 className="text-lg">Add Step</h4>
+          </li>
+
+          {/* Set Reminder */}
+          <li className="border-t flex items-center space-x-4 pt-4">
+            <FaRegBell className="text-2xl" />
+            <h4 className="text-lg">Set Reminder</h4>
+          </li>
+
+          {/* Add Due Date */}
+          <li className="border-t flex items-center justify-between pt-4 cursor-pointer" onClick={() => setShowDatePicker(!showDatePicker)}>
+            <div className="flex items-center space-x-4">
+              <CiCalendar className="text-2xl" />
+              <h4 className="text-lg">{dueDate ? dueDate.toDateString() : "Add Due Date"}</h4>
             </div>
+          </li>
+          
+          {/* Date Picker */}
+          {showDatePicker && (
+            <div className="ml-10 mt-2 bg-green-100">
+              <DatePicker
+                selected={dueDate}
+                onChange={(date) => {
+                  setDueDate(date);
+                  setShowDatePicker(false);
+                }}
+                inline
+              />
+            </div>
+          )}
 
-        </div>
-    )
-}
+          {/* Repeat */}
+          <li className="border-t flex items-center space-x-4 pt-4">
+            <BsRepeat className="text-2xl" />
+            <h4 className="text-lg">Repeat</h4>
+          </li>
+
+          {/* Add Notes */}
+          <li className="border-t pt-4 pb-32">
+            <input
+              className="w-full bg-transparent outline-none text-lg placeholder-gray-500"
+              placeholder="Add Notes"
+            />
+          </li>
+        </ul>
+      </div>
+
+      {/* Footer */}
+      <div className="border-t flex justify-between items-center p-4">
+        <ImCross className="text-lg cursor-pointer" />
+        <h4 className="text-lg text-gray-500">Created Today</h4>
+        <PiTrashFill className="text-2xl text-red-500 cursor-pointer hover:text-red-700" />
+      </div>
+    </div>
+  );
+};
 
 export default Action;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -28,45 +28,68 @@ const Todo = () => {
   };
 
   return (
-        <div className={`${!isProfileVisible && isActionVisible ? "w-[52vw] ml-[1vw] mr-[1vw]" : !isActionVisible && !isProfileVisible ? "ml-[2vw] mr-[2vw]" : !isActionVisible && isProfileVisible ? "w-[75vw] col-span-4 -ml-[12vw] " : " -ml-[13vw]"}`} >
-            {/* Heading */}
-            <div className="flex items-end border-b-2 border-gray-300 pt-[7vh] pb-[1vh] dark:bg-black dark:text-white ">
-                <h1 className="text-lg font-semibold">To Do</h1>
-                <h1 className=" text-lg">
-                    <MdArrowDropDown />
-                </h1>
-            </div>
-            {/* add task  */}
-            <form onSubmit={handleAddTodo}>
-                <div className="border-b-2  ">
-                    <input type="text" onChange={handleChangeData} className="bg-transparent outline-none h-[23vh]  text-4xl w-full pl-[2vw] placeholder:text-3xl placeholder:pl-[2vw]" placeholder="Add A Task" />
-                    <div className="flex justify-between">
-                        {/* icons */}
-                        <div className="text-3xl  flex space-x-8 ml-[2vw] cursor-pointer">
-                            <FaRegBell />
-                            <BsRepeat />
-                            <CiCalendar />
+    
+    <div
+  className={`
+    transition-all duration-300
+    ${!isProfileVisible && isActionVisible 
+      ? "lg:w-[52vw] lg:ml-[1vw] lg:mr-[1vw]" 
+      : !isActionVisible && !isProfileVisible 
+        ? "lg:ml-[2vw] lg:mr-[2vw] "
+        : !isActionVisible && isProfileVisible 
+          ? "w-[70vw] col-span-4 -ml-[12vw]"
+          : "-ml-[12vw] "}
+  `}
+>
 
-                        </div >
-                        <button type="submit" className="bg-[#357937] text-white font-semibold py-2 px-4 rounded-md shadow-lg m-[1vh] " >
-                            ADD TASK
-                        </button>
-                    </div>
-                </div>
-            </form>
-            {/*list of todo */}
-            <div className="w-full ">
-                <TodoItem />
-            </div>
-            {/* compeleted todo */}
-            <div className="w-full">
-                <h1>Compeleted</h1>
-                <CompeletedTodo />
+      {/* Heading */}
+      <div className="flex items-end border-b-1 border-gray-300 pt-[7vh] pb-[1vh] dark:bg-black dark:text-white">
+        <h1 className="text-lg font-semibold">To Do</h1>
+        <h1 className="text-lg">
+          <MdArrowDropDown />
+        </h1>
+      </div>
+
+      {/* Add Task */}
+      <form onSubmit={handleAddTodo}>
+        <div className="border-b-1 bg-[#EEF6EF] p-4 dark:text-white dark:bg-black flex flex-col">
+          <input
+            type="text"
+            onChange={handleChangeData}
+            className="bg-transparent outline-none h-[23vh] md:h-[20vh] text-2xl md:text-4xl w-full pl-[2vw] placeholder:text-xl md:placeholder:text-3xl placeholder:pl-[2vw]"
+            placeholder="Add A Task"
+          />
+        
+          <div className="flex justify-between items-center">
+            {/* Icons */}
+            <div className="text-2xl md:text-3xl flex space-x-6 md:space-x-8 ml-[2vw] cursor-pointer">
+              <FaRegBell />
+              <BsRepeat />
+              <CiCalendar />
             </div>
 
-
+            <button
+              type="submit"
+              className="bg-[#357937] text-white font-semibold py-2 px-4 rounded-md shadow-lg m-[1vh] text-sm md:text-base"
+            >
+              ADD TASK
+            </button>
+          </div>
         </div>
-    )
-}
+      </form>
 
-export default Todo
+      {/* List of To-Do */}
+      <div className="w-full">
+        <TodoItem />
+      </div>
+
+      {/* Completed To-Do */}
+      <div className="w-full mt-4">
+        <h1 className="text-lg font-semibold">Completed</h1>
+        <CompeletedTodo />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
